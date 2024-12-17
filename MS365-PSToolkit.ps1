485ec95d5eb25cc043d1b054b0b0fe7034fb57dc
N30X420 <83133061+N30X420@users.noreply.github.com>
2024-12-17 12:54:37 +0100

    Update MS365-PSToolkit.ps1

--- MS365-PSToolkit.ps1
+++ MS365-PSToolkit.ps1
@@ -3,19 +3,14 @@
 #--------------------------------------
 $version = "2.0-alpha"
 $ProgramName = "MS365-PSToolkit"
-########################################
-#$tempdir = "C:\INSTALL\$ProgramName-$version"
+$tempdir = "C:\INSTALL\$ProgramName-$version"
 #######################################
 #######################################
-# Fixed Variables
-#--------------------------------------
 $error.clear()
 $ProgressPreference = 'SilentlyContinue'
 $host.UI.RawUI.WindowTitle = "$ProgramName - Version $version"
-#$curDate = Get-Date -Format "dd-MM-yyyy-HH-mm-ss"
 [console]::WindowWidth=200; [console]::WindowHeight=50; [console]::BufferWidth=[console]::WindowWidth
 #######################################
-
 
 
 function Logo {
@@ -26,12 +21,13 @@ function Logo {
     Write-Host " |_|  |_|___/___/\___/___/   |_|  |___/ |_|\___/\___/_|_\_\_|\__|" -ForegroundColor Blue
     Write-Host ""
     write-Host "v$version" -ForegroundColor Blue
+    Write-Host
+    Write-Host "Microsoft 365 Powershell Toolkit" -ForegroundColor Cyan
+    Write-Host "MATRIXNET ~ Vincent" -ForegroundColor Cyan
+    Write-Host ""
 }
-
-
-
 function Show-MainMenu {
-    Write-Host "#### MAIN MENU ####" -ForegroundColor Cyan
+    Write-Host "#### MAIN MENU ####" -ForegroundColor DarkCyan
     Write-Host "`nSelect Microsoft 365 Service To Connect With"
     Write-Host "`n(1) - Microsoft Graph"
     Write-Host "(2) - Exchange Online"
@@ -39,9 +35,40 @@ function Show-MainMenu {
     Write-Host "`n(9) - Exit"
     Write-Host ""
 }
+function CheckPowershellVersion {
+    if ($PSVersionTable.PSVersion.Major -ne 7){
+        Write-Warning "This program requires powershell 7. You are running version $($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor) Please Update to Powershell 7.x"
+        exit
+    }
+}
+function CheckAdminPrivs {
+    function isadmin {
+        # Returns true/false
+        ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+    }
+        if (isadmin -eq "True") {
+            Write-Host "`nGot Administrator Permissions" -ForegroundColor Green
+            Start-Sleep -Seconds 1
+        }
+        else {
+            Write-Error "`nThis script needs Administrator Privileges to work it's magic"
+            Start-Sleep -Seconds 3
+        }
+        if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+            if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+                $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+                Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+                Exit
+            }
+        }
+}
 
+
+####################################
+#  MsGraph  #
+#-------------
 function Show-MsGraphMenu {
-    Write-Host "#### Microsoft Graph Menu ####" -ForegroundColor Cyan
+    Write-Host "#### Microsoft Graph Menu ####" -ForegroundColor DarkCyan
     Write-Host "(1) - Install Required Modules"
     Write-Host "(2) - Check required permission scopes for MsGraph Command"
     Write-Host "(3) - Tool - Forced password change at next login - all users"
@@ -50,30 +77,6 @@ function Show-MsGraphMenu {
     Write-Host "`n(9) - Main Menu"
     Write-Host ""
 }
-
-function Show-ExchangeOnlineMenu {
-    Write-Host "#### Microsoft Exchange Online Menu ####" -ForegroundColor Cyan
-    Write-Host "`n(1) - List Mailbox"
-    Write-Host "(2) - List Mailbox Permissions"
-    Write-Host "(3) - List Specific Mailbox Permissions"
-    Write-Host "(4) - List Mailbox Size"
-    Write-Host "(8) - Custom Command"
-    Write-Host "`n(9) - Main Menu"
-    Write-Host ""
-}
-
-
-function installExchangeOnlineModule {
-    if(-not (Get-Module ExchangeOnlineManagement -ListAvailable)){
-        Write-Warning "Module ExchangeOnlineManagement not installed"
-        Write-Host "Installing ExchangeOnlineManagement Please Wait" -ForegroundColor Green
-        Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force 
-        }
-    Start-Sleep -Seconds 1
-    Write-Host "Module Already Installed" -ForegroundColor Green
-    Start-Sleep -Seconds 2
-}
-
 function installMicrosoftGraphModule {
     if(-not (Get-Module Microsoft.Graph -ListAvailable)){
         Write-Warning "Module Microsoft.Graph not installed"
@@ -82,16 +85,9 @@ function installMicrosoftGraphModule {
         Install-Module Microsoft.Graph.Beta -Scope CurrentUser -Force
         }
     Start-Sleep -Seconds 1
-    Write-Host "Module Already 
-    Installed" -ForegroundColor Green
+    Write-Host "Module Already Installed" -ForegroundColor Green
     Start-Sleep -Seconds 2
 }
-
-
-
-
-
-####################################
 function CheckMsGraphDelegatedPermissions {
     $exit = 0
     while ($exit -eq 0){
@@ -105,7 +101,6 @@ function CheckMsGraphDelegatedPermissions {
         }
     }
 }
-
 function MsGraphForcePasswordResetAllUsers {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
@@ -169,16 +164,178 @@ function MsGraphForcePasswordResetAllUsers {
     Write-Progress -Activity "Processing Users" -Completed
     
 }
+function MsGraphForcePasswordResetSingleUser {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    if (Get-MgContext) {
+        Write-Host Disconnecting from the previous sesssion.... -ForegroundColor Yellow
+        Disconnect-MgGraph | Out-Null
+    }
+    Connect-MgGraph -Scopes "User.Read.All", "User-PasswordProfile.ReadWrite.All", "UserAuthenticationMethod.ReadWrite.All" -NoWelcome
+    Write-Host "Enter UPN of account to configure forced password change at next login (example@domain.com)"
+        $UPN = Read-Host "UPN :"
+        $MSAccount = Get-MgBetaUser -UserId $UPN
+    try {
+        Update-MgUser -UserId $MSAccount.id -PasswordProfile @{ForceChangePasswordNextSignIn=$true}
+        Write-Host "$($UMSAccount.UserPrincipalName) Configured" -ForegroundColor Green
+        Start-Sleep -Seconds 2
+    }
+    catch {
+        Write-Warning "Error updating $($UMSAccount.UserPrincipalName)"
+    }
+    Disconnect-MgGraph | Out-Null
+}
+function CreateMFAStatusReport {
+    #Disconnect from the Microsoft Graph If already connected
+    if (Get-MgContext) {
+        Write-Host Disconnecting from the previous sesssion.... -ForegroundColor Yellow
+        Disconnect-MgGraph | Out-Null
+    }
 
+    # Connect to Microsoft Graph API
+    Connect-MgGraph -Scopes "User.Read.All", "UserAuthenticationMethod.Read.All", "UserAuthenticationMethod.ReadWrite.All" -NoWelcome
+
+    # Create variable for the date stamp
+    $LogDate = Get-Date -f yyyyMMddhhmm
+
+    # Define CSV file export location variable
+    $Csvfile = "$tempdir\MFAUsers_$LogDate.csv"
+
+    # Get all Microsoft Entra ID users using the Microsoft Graph Beta API
+    $users = Get-MgBetaUser -All
+
+    # Initialize a List to store the data
+    $Report = [System.Collections.Generic.List[Object]]::new()
+
+    # Initialize progress counter
+    $counter = 0
+    $totalUsers = $users.Count
+
+    # Loop through each user account
+    foreach ($user in $users) {
+        $counter++
+
+        # Calculate percentage completion
+        $percentComplete = [math]::Round(($counter / $totalUsers) * 100)
+
+        # Define progress bar parameters with user principal name
+        $progressParams = @{
+            Activity        = "Processing Users"
+            Status          = "User $($counter) of $totalUsers - $($user.UserPrincipalName) - $percentComplete% Complete"
+            PercentComplete = $percentComplete
+        }
+
+        Write-Progress @progressParams
+
+        # Create an object to store user MFA information
+        $ReportLine = [PSCustomObject]@{
+            DisplayName               = "-"
+            UserPrincipalName         = "-"
+            MFAstatus                 = "Disabled"
+            DefaultMFAMethod          = "-"
+            Email                     = "-"
+            Fido2                     = "-"
+            MicrosoftAuthenticatorApp = "-"
+            Phone                     = "-"
+            SoftwareOath              = "-"
+            TemporaryAccessPass       = "-"
+            WindowsHelloForBusiness   = "-"
+        }
+
+        $ReportLine.UserPrincipalName = $user.UserPrincipalName
+        $ReportLine.DisplayName = $user.DisplayName
+
+        # Check authentication methods for each user
+        $MFAData = Get-MgBetaUserAuthenticationMethod -UserId $user.Id
+
+        # Retrieve the default MFA method
+        $DefaultMFAUri = "https://graph.microsoft.com/beta/users/$($user.Id)/authentication/signInPreferences"
+        try {
+            $DefaultMFAMethod = Invoke-MgGraphRequest -Uri $DefaultMFAUri -Method GET
+            if ($DefaultMFAMethod.userPreferredMethodForSecondaryAuthentication) {
+                $ReportLine.DefaultMFAMethod = $DefaultMFAMethod.userPreferredMethodForSecondaryAuthentication
+            }
+            else {
+                $ReportLine.DefaultMFAMethod = "Not set"
+            }
+        }
+        catch {
+            Write-Warning "Failed to retrieve default MFA method for $($user.UserPrincipalName): $_"
+            $ReportLine.DefaultMFAMethod = "Error"
+        }
+
+        foreach ($method in $MFAData) {
+
+            Switch ($method.AdditionalProperties["@odata.type"]) {
+                "#microsoft.graph.emailAuthenticationMethod" {
+                    $ReportLine.Email = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.fido2AuthenticationMethod" {
+                    $ReportLine.Fido2 = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.microsoftAuthenticatorAuthenticationMethod" {
+                    $ReportLine.MicrosoftAuthenticatorApp = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.phoneAuthenticationMethod" {
+                    $ReportLine.Phone = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.softwareOathAuthenticationMethod" {
+                    $ReportLine.SoftwareOath = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.temporaryAccessPassAuthenticationMethod" {
+                    $ReportLine.TemporaryAccessPass = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+                "#microsoft.graph.windowsHelloForBusinessAuthenticationMethod" {
+                    $ReportLine.WindowsHelloForBusiness = $true
+                    $ReportLine.MFAstatus = "Enabled"
+                }
+            }
+        }
+        # Add the report line to the List
+        $Report.Add($ReportLine)
+    }
+
+    # Clear progress bar
+    Write-Progress -Activity "Processing Users" -Completed
+
+    # Export user information to CSV
+    $Report | Export-Csv -Path $Csvfile -NoTypeInformation -Encoding UTF8
+
+    Write-Host "Script completed. Results exported to $Csvfile." -ForegroundColor Cyan
+
+    Disconnect-MgGraph | Out-Null
+    
+}
 ####################################
 
-
-
-
-
-
-
-
+####################################
+#  Exchange Online  #
+#-----------------------
+function Show-ExchangeOnlineMenu {
+    Write-Host "#### Microsoft Exchange Online Menu ####" -ForegroundColor DarkCyan
+    Write-Host "`n(1) - List Mailbox"
+    Write-Host "(2) - List Mailbox Permissions"
+    Write-Host "(3) - List Specific Mailbox Permissions"
+    Write-Host "(4) - List Mailbox Size"
+    Write-Host "(8) - Custom Command"
+    Write-Host "`n(9) - Main Menu"
+    Write-Host ""
+}
+function installExchangeOnlineModule {
+    if(-not (Get-Module ExchangeOnlineManagement -ListAvailable)){
+        Write-Warning "Module ExchangeOnlineManagement not installed"
+        Write-Host "Installing ExchangeOnlineManagement Please Wait" -ForegroundColor Green
+        Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force 
+        }
+    Start-Sleep -Seconds 1
+    Write-Host "Module Already Installed" -ForegroundColor Green
+    Start-Sleep -Seconds 2
+}
 function isExchangeConnected {
     $Connection = Get-ConnectionInformation
     If (-Not ( $Connection.State -match 'Connected' ) ){
@@ -191,8 +348,6 @@ function isExchangeConnected {
         Disconnect-ExchangeOnline -Confirm:$false
     }
 }
-
-
 function customExchangeCmd {
     $whileLoopVarCustomCmd = 1
     Logo
@@ -209,6 +364,9 @@ function customExchangeCmd {
         }       
     }    
 }
+####################################
+
+
 
 
 
@@ -218,52 +376,34 @@ function customExchangeCmd {
 # Main Code --- Main Code --- Main Code --- Main Code --- Main Code #
 #-------------------------------------------------------------------#
 Logo
-Write-Host
-Write-Host "Microsoft 365 Powershell Toolkit" -ForegroundColor Yellow
-Write-Host "`nMATRIXNET ~ Vincent" -ForegroundColor Yellow
-Write-Host
-Write-Host "----------------------------" -ForegroundColor Magenta
-write-Host "| Always trust the process |" -ForegroundColor Magenta
-Write-Host "----------------------------" -ForegroundColor Magenta
-Start-Sleep -Seconds 3
+Start-Sleep -Seconds 1
 Set-ExecutionPolicy Bypass -Force -Scope Process -Confirm:$false
 #####################################################################
 
-#########################
-# Check admin rights    #
-#-----------------------#
-function isadmin
- {
- # Returns true/false
-   ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
- }
- if (isadmin -eq "True") {
-     Write-Host "`nGot Administrator Permissions" -ForegroundColor Green
-     Start-Sleep -Seconds 1
- }
- else {
-     Write-Host "`nThis script needs Administrator Privileges to work it's magic" -ForegroundColor Red
-     Start-Sleep -Seconds 3
- }
- if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-  if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-   $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
-   Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-   Exit
-  }
-}
 #####################################################################
 
+######################
+# Requirements Check #
+######################
+CheckPowershellVersion
+CheckAdminPrivs
 
+if (!(Test-Path $tempdir)) { 
+    Write-Error "`n`nLog Directory not found"
+    Write-Warning "Creating Log Directory in $tempdir"
+    New-Item -itemType Directory -Path $logdir | Out-Null
+}
+######################
 
-
-
-
-# Begin Loop
+##############
+# Loop Menus #
+##############
 $WhileLoopVarMainMenu = 1
 while ($WhileLoopVarMainMenu -eq 1){
     Clear-Host
-    Start-Sleep -Milliseconds 500
+    Start-Sleep -Milliseconds 250
+    Logo
+    Start-Sleep -Milliseconds 250
     Show-MainMenu
     $MainMenuChoice = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
     switch ($MainMenuChoice) {
@@ -271,13 +411,17 @@ while ($WhileLoopVarMainMenu -eq 1){
             $WhileLoopVarMsGraphMenu = 1
             while ($WhileLoopVarMsGraphMenu -eq 1){
                 Clear-Host
-                Start-Sleep -Milliseconds 500
+                Start-Sleep -Milliseconds 250
+                Logo
+                Start-Sleep -Milliseconds 250
                 Show-MsGraphMenu
                 $MsGraphMenuChoice = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
                     switch ($MsGraphMenuChoice) {
                         49 {installMicrosoftGraphModule}
                         50 {CheckMsGraphDelegatedPermissions}
                         51 {MsGraphForcePasswordResetAllUsers}
+                        52 {MsGraphForcePasswordResetSingleUser}
+                        53 {CreateMFAStatusReport}
                         57 {
                             Write-Host "`nReturning to Main Menu" -ForegroundColor Cyan
                             $WhileLoopVarMsGraphMenu = 0
@@ -295,8 +439,10 @@ while ($WhileLoopVarMainMenu -eq 1){
             $WhileLoopVarExchangeOnlineMenu = 1
             while ($WhileLoopVarExchangeOnlineMenu -eq 1){
                 Clear-Host
-                Start-Sleep -Milliseconds 500
-                Show-MsGraphMenu
+                Start-Sleep -Milliseconds 250
+                Logo
+                Start-Sleep -Milliseconds 250
+                Show-ExchangeOnlineMenu
                 $MsGraphMenuChoice = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
                     switch ($MsGraphMenuChoice) {
                         49 {installExchangeOnlineModule}
@@ -313,10 +459,10 @@ while ($WhileLoopVarMainMenu -eq 1){
                     }
             }
         }
-        51 { # ASCII for "3"
+        51 {
             Write-Host "3"
         }
-        57 { # ASCII for "9"
+        57 {
             Write-Host "`nExiting... Goodbye!" -ForegroundColor Cyan
             $WhileLoopVarMainMenu = 0
             exit
@@ -329,7 +475,8 @@ while ($WhileLoopVarMainMenu -eq 1){
 
     if ($MainMenuChoice -ne 57) {
         Write-Host ""
-        Start-Sleep -Seconds 2
+        Start-Sleep -Milliseconds 250
     }
 }
 exit
+##############
