2e0c3ea27f817d32ad6eb06e2c263e58f2a53f47
N30X420 <83133061+N30X420@users.noreply.github.com>
2024-12-18 17:11:12 +0100

    Update MS365-PSToolkit.ps1
    Added Exchange Online List mailbox
    Added Export to CSV to List mailbox
    Added Submenu Exchange Online Tools

--- MS365-PSToolkit.ps1
+++ MS365-PSToolkit.ps1
@@ -11,6 +11,7 @@ $error.clear()
 $ProgressPreference = 'Continue'
 $host.UI.RawUI.WindowTitle = "$ProgramName - Version $version"
 [console]::WindowWidth=200; [console]::WindowHeight=50; [console]::BufferWidth=[console]::WindowWidth
+$script:ExportCSV = $false
 #######################################
 
 
@@ -107,6 +108,19 @@ function CatchError {
     Write-Host -NoNewLine "Press any key to return to the menu..." -ForegroundColor Yellow
     $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
 }
+function PromptExportToCSV {
+    Write-Host "Export data to CSV ? [y/n]" -ForegroundColor Yellow
+    $UserInput = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    If ($UserInput -eq "y"){
+        Write-Host "Exporting Data" -ForegroundColor Yellow
+        $script:ExportCSV = $true
+    }
+    else {
+        Start-Sleep -Seconds 1
+        Break
+    }
+}
+
 ####################################
 #  MsGraph  #
 #-------------
@@ -157,7 +171,7 @@ function MsGraphForcePasswordResetAllUsers {
     Write-Warning "This script will force every account to change password at next sign in."
     Write-Host "`nContinue ? [y/n]"
     $continue = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-    if ($continue.Character -eq "n"){
+    if ($continue.Character -ine "y"){
         Break
     }
     
@@ -383,9 +397,19 @@ function Show-ExchangeOnlineMenu {
     Write-Host "(4) - List Mailbox Permissions - Coming Soon" -ForegroundColor Red
     Write-Host "(5) - List Specific Mailbox Permissions - Coming Soon" -ForegroundColor Red
     Write-Host "(6) - List Mailbox Size - Coming Soon" -ForegroundColor Red
+    Write-Host "(7) - Open Tools Menu"
     Write-Host "(8) - Custom Command"
     Write-Host "`n(9) - Main Menu"
     Write-Host "################################################" -ForegroundColor DarkCyan
+    Write-Host ""
+}
+function Show-ExchangeOnlineToolsMenu {
+    Write-Host "######## Microsoft Exchange Online Tools Menu ########" -ForegroundColor DarkCyan
+    Write-Host "Exchange Online Connection : " -NoNewline -ForegroundColor Yellow
+    Write-Host "$connectionStatus" -ForegroundColor $connectionStatusColor
+    Write-Host "`n(1) - Enable Delegate Sent Items Shared Mailbox"
+    Write-Host "`n(9) - Return to Exchange Online Menu"
+    Write-Host "#####################################################" -ForegroundColor DarkCyan
     Write-Host ""
 }
 function installExchangeOnlineModule {
@@ -412,10 +436,39 @@ function ExchangeOnlineConnection {
 }
 
 function ExchangeOnlineListMailbox {
-    
-    
-}
+    Get-Recipient -ResultSize Unlimited -RecipientTypeDetails UserMailbox,SharedMailbox | select-object Displayname,RecipientType,PrimarySMTPAddress,EmailAddresses
+    PromptExportToCSV
+    if ($script:ExportCSV -eq $true){
+        $RecipientType={'RoomMailbox', 'LinkedRoomMailbox', 'EquipmentMailbox', 'SchedulingMailbox', 
+        'LegacyMailbox', 'LinkedMailbox', 'UserMailbox', 'MailContact', 'DynamicDistributionGroup', 'MailForestContact', 'MailNonUniversalGroup', 'MailUniversalDistributionGroup', 'MailUniversalSecurityGroup', 
+        'RoomList', 'MailUser', 'GuestMailUser', 'GroupMailbox', 'DiscoveryMailbox', 'PublicFolder', 'TeamMailbox', 'SharedMailbox', 'RemoteUserMailbox', 'RemoteRoomMailbox', 'RemoteEquipmentMailbox', 
+        'RemoteTeamMailbox', 'RemoteSharedMailbox', 'PublicFolderMailbox', 'SharedWithMailUser'}
 
+        $ExportResult=""   
+        $ExportResults=@()  
+        $OutputCSV="$tempdir\Office365EmailAddressesReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv"
+        $Count=0
+        #Get all Email addresses in Microsoft 365
+        Get-Recipient -ResultSize Unlimited -RecipientTypeDetails $RecipientType | ForEach-Object {
+            $Count++
+            $DisplayName=$_.DisplayName
+            Write-Progress -Activity "`n     Retrieving email addresses of $DisplayName.."`n" Processed count: $Count"
+            $RecipientTypeDetails=$_.RecipientTypeDetails
+            $PrimarySMTPAddress=$_.PrimarySMTPAddress
+            $Alias= ($_.EmailAddresses | Where-Object {$_ -clike "smtp:*"} | ForEach-Object {$_ -replace "smtp:",""}) -join ","
+            If($Alias -eq "")
+            {
+            $Alias="-"
+            }
+
+            #Export result to CSV file
+            $ExportResult=@{'Display Name'=$DisplayName;'Recipient Type Details'=$RecipientTypeDetails;'Primary SMTP Address'=$PrimarySMTPAddress;'Alias'=$Alias}
+            $ExportResults= New-Object PSObject -Property $ExportResult  
+            $ExportResults | Select-Object 'Display Name','Recipient Type Details','Primary SMTP Address','Alias' | Export-Csv -Path $OutputCSV -Notype -Append
+        }
+    }
+    Write-Host "Script completed. Results exported to  $OutputCSV." -ForegroundColor Cyan
+}
 function ExchangeOnlineSaveSentItemsInSharedMailboxAll {
     $mailboxes = (get-mailbox -ResultSize Unlimited -RecipientTypeDetails Sharedmailbox)
     $counter = 0
@@ -434,7 +487,6 @@ function ExchangeOnlineSaveSentItemsInSharedMailboxAll {
     Write-Progress -Activity "Processing Mailboxes" -Completed
         
 }
-
 function customExchangeCmd {
     $whileLoopVarCustomCmd = 1
     Clear-Host
@@ -557,10 +609,30 @@ while ($WhileLoopVarMainMenu -eq 1){
                     50 {try {ExchangeOnlineConnection}
                         catch {Write-Error "Error while connecting"
                             CatchError}}
-                    51 {}
+                    51 {try {ExchangeOnlineListMailbox}
+                        catch {CatchError}}
                     52 {}
                     53 {}
-                    54 {}
+                    54 {
+                        $WhileLoopVarExchangeOnlineToolsMenu = 1
+                        while ($WhileLoopVarExchangeOnlineToolsMenu -eq 1){
+                            Clear-Host
+                            Start-Sleep -Milliseconds 250
+                            Logo
+                            Start-Sleep -Milliseconds 250
+                            Show-ExchangeOnlineToolsMenu
+                            $MsGraphMenuChoice = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").VirtualKeyCode
+                            switch ($MsGraphMenuChoice) {
+                                49 {try {ExchangeOnlineSaveSentItemsInSharedMailboxAll}
+                                    catch {CatchError}}
+                                57 {Write-Host "`nReturning to Exchange Online Menu" -ForegroundColor Cyan
+                                $WhileLoopVarExchangeOnlineToolsMenu = 0}
+                                
+                                default {Write-Host "`nInvalid selection, please try again." -ForegroundColor Red
+                                    Start-Sleep -Seconds 1}
+                            }
+                        }
+                    }
                     56 {customExchangeCmd}
                     57 {Write-Host "`nReturning to Main Menu" -ForegroundColor Cyan
                         $WhileLoopVarExchangeOnlineMenu = 0}
